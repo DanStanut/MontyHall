d434bc8f454fb6ad202c3ad661edcd9b43ead845
DanStanut <dan.stanut@gmail.com>
2023-10-07 13:37:32 +0300

    optimised look

--- script.js
+++ script.js
@@ -8,35 +8,36 @@ function buttonPressed() {
     } else {
         document.getElementById("infoText").innerHTML = "Goat behind this door"
         this.className = "btn btn-secondary disabled"
-        //this.mouseup = buttonReleased
     }
 }
 
 function generateButtons(noBut) {
     var myCol = document.getElementById("buttonsCol")
-    var myRow = document.getElementById("buttonsRow")
     document.getElementById("startButton").className = "btn btn-secondary disabled"
     for (let i = 0; i < noBut; ++i) {
         var btn = document.createElement("button")
-        var column = document.createElement("div")  
+        var column = document.createElement("div")
+        var centerB = document.createElement("div")
         btn.className = "btn btn-info"
         btn.onclick = buttonPressed
         btn.innerHTML = "Pick ME"
         btn.id = i
         column.className = "col"
-        column.appendChild(btn)
-        myRow.appendChild(column)
+        centerB.className = "text-center"
+        centerB.appendChild(btn)
+        column.appendChild(centerB)
+        document.getElementById("buttonsRow").appendChild(column)
     }
 }
 
 function validateInput() {
-    var enteredNumber = document.getElementById("numberOfDoors").value
+    var enteredNumber = document.getElementById("numberOfButtons").value
     if (enteredNumber < 2 || enteredNumber > 10) {
         alert("Please give a number between 2 and 10");
     } else {
-        var numberOfButtons = enteredNumber
-        winner = Math.round(Math.random() * numberOfButtons)
+        enteredNumber
+        winner = Math.round(Math.random() * (enteredNumber - 1))
         document.getElementById("infoText").innerHTML = "Please choose a button below!"
-        generateButtons(numberOfButtons)
+        generateButtons(enteredNumber)
     }
 }
