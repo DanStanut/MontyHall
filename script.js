9eb43440f778752f59edfae879a11f72b49af8a1
Dan STANUT <60068096+DanStanut@users.noreply.github.com>
2023-10-24 16:39:37 +0300

    Button id passed as a parameter

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-var winner
+var winner;
 
-function buttonPressed() {
-    let pressedButtonId = this.id
+function buttonPressed(pressedButtonId) {
     if (pressedButtonId == winner) {
-        document.getElementById("infoText").innerHTML = "You WIN !!!"
-        this.className = "btn btn-success"
+        document.getElementById("infoText").innerHTML = "You WIN !!!";
+        document.getElementById("Button_" + pressedButtonId).className = "btn btn-success";
     } else {
-        document.getElementById("infoText").innerHTML = "Goat behind this door"
-        this.className = "btn btn-secondary disabled"
+        document.getElementById("infoText").innerHTML = "Goat behind this door";
+        document.getElementById("Button_" + pressedButtonId).className = "btn btn-secondary disabled";
     }
 }
 
 function generateButtons(noBut) {
-    document.getElementById("startButton").className = "btn btn-secondary disabled"
+    document.getElementById("startButton").className = "btn btn-secondary disabled";
     for (let i = 0; i < noBut; ++i) {
-        let btn = document.createElement("button")
-        let column = document.createElement("div")
-        let centerB = document.createElement("div")
-        btn.className = "btn btn-info"
-        btn.id = i
-        btn.onclick = buttonPressed
-        btn.innerHTML = "Pick ME"
-        column.className = "col"
-        centerB.className = "text-center"
-        centerB.appendChild(btn)
-        column.appendChild(centerB)
-        document.getElementById("buttonsRow").appendChild(column)
+        var btn = document.createElement("button");
+        var column = document.createElement("div");
+        var centerB = document.createElement("div");
+        btn.className = "btn btn-info";
+        btn.innerHTML = "Pick ME";
+        btn.id = "Button_" + i;
+        btn.onclick = function() {
+            buttonPressed(i);
+        };
+        column.className = "col";
+        centerB.className = "text-center";
+        centerB.appendChild(btn);
+        column.appendChild(centerB);
+        document.getElementById("buttonsRow").appendChild(column);
     }
 }
 
 function validateInput() {
-    let enteredNumber = document.getElementById("numberOfButtons").value
+    var enteredNumber = document.getElementById("numberOfButtons").value;
     if (enteredNumber < 2 || enteredNumber > 10) {
         alert("Please give a number between 2 and 10");
     } else {
-        winner = Math.round(Math.random() * (enteredNumber - 1))
-        document.getElementById("infoText").innerHTML = "Please choose a button below!"
-        generateButtons(enteredNumber)
+        winner = Math.round(Math.random() * (enteredNumber - 1));
+        document.getElementById("infoText").innerHTML = "Please choose a button below!";
+        generateButtons(enteredNumber);
     }
 }
